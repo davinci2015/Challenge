8eb2f6efbff6dbb66fcca648f8b8146145478a86
Igor Tepavac <igotepava@foi.hr>
2015-09-24 21:40:14 +0200

    Poolishing

--- challenge-client/app/src/main/java/hr/foi/challenge/challengeclient/activities/RankActivity.java
+++ challenge-client/app/src/main/java/hr/foi/challenge/challengeclient/activities/RankActivity.java
@@ -7,7 +7,7 @@ import hr.foi.challenge.challengeclient.R;
 
 public class RankActivity extends BaseActivity {
 
-    private static final String URL = "http://46.101.207.199/";
+    private static final String URL = "http://46.101.207.199/admin/ajax/get_rank_list.php?server_call=1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
